95470764614489a237c59cc9fffa1e0b944e6467
NGUEND-ARTHUR <nguend.johann@ictuniversity.edu.cm>
2025-12-01 15:52:15 +0100

    Eighth Commit

---
+++ backend/src/main/java/com/smartcampost/backend/model/enums/PaymentOption.java
@@ -0,0 +1,6 @@
+package com.smartcampost.backend.model.enums;
+
+public enum PaymentOption {
+    PREPAID,  // le client paye avant l’acheminement
+    COD       // Cash On Delivery (paiement à la remise)
+}
